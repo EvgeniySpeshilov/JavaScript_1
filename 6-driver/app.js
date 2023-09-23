8096387acd4da38472c52adb9c434830b6fab0d9
EvgeniySpeshilov <amurdv@yandex.ru>
2023-09-24 02:50:43 +1000

    Код  ревью 2

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -2,7 +2,7 @@
 
 let age = 18;
 let hasLicence = true;  //  Говорит о наличии прав
-let isDrunk = false;     //  Показывает что пьян, если true
+let isDrunk = false;     //  Показывает что пьян, если true.
 
 // Основная строка кода , где проверяем все парамметры и если они выполняются, значит 'может'
 const canDrive = age >= 18 && hasLicence && !isDrunk;
---
+++ 9-sort-loops/app.js
@@ -0,0 +1,18 @@
+const arr = [1, 40, -5, 10, 0, 7, 27, -10];   
+
+function boobleSort(arr) {                  // Задаем  функцию.
+
+for (let i = 0; i < arr.length; i++) {      // Вызываем цикл сравнения столько раз, сколько элементов массива
+for (let j = i; j < arr.length; j++) {      // Проходим по всем элементам массива
+	if (arr[i] < arr[j]) { 
+		console.log(arr[i], arr[j]);               // сравниваем 2 соседних элимента
+		let temp = arr[i];                  // после сравнения 2х элементов, 
+		arr[i] = arr[j];                    // меняем их местами.
+        arr[j] = temp; 
+	   }   
+    } 
+ }
+    return arr; 
+}	
+
+console.log(boobleSort(arr));
