3ca7977c0f76cf6e179d8f7f7b855566f6592f65
EvgeniySpeshilov <amurdv@yandex.ru>
2023-09-25 12:49:32 +1000

    Подправил табуляцию

--- 7-converter/app.js
+++ 7-converter/app.js
@@ -7,17 +7,17 @@ function curencyConverter(amountOffunds, curencyOffunds, targetCurency) {
 	if (curencyOffunds === 'rub') { 
 		switch (targetCurency) {
 		case 'usd':                                        // if (targetCurency === 'usd')
-		      const priseUSD = (amountOffunds / 98);
-			  return `${priseUSD} ${targetCurency}`;
+			const priseUSD = (amountOffunds / 98);
+			return `${priseUSD} ${targetCurency}`;
 		case 'eur':
-		      const priseEUR = (amountOffunds / 103);
-	          return `${priseEUR} ${targetCurency}`;
+		    const priseEUR = (amountOffunds / 103);
+	        return `${priseEUR} ${targetCurency}`;
 	    case 'cny':
-		      const priseCNY = ((amountOffunds / 132) * 10);
-	          return `${priseCNY} ${targetCurency}`;
+		    const priseCNY = ((amountOffunds / 132) * 10);
+	        return `${priseCNY} ${targetCurency}`;
 		
 		default:
-		      return   'Null';
+		    return   'Null';
 		} 
 	} else {
 		console.log('Null');
